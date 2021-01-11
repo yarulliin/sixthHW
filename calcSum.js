d044c1fbc2fff5a8e53304a0e53178939481cc16
Rifat Yarullin <rifat.jarullin@gmail.com>
2021-01-12 00:36:50 +0400

    conditions have been changed

--- calcSum.js
+++ calcSum.js
@@ -2,25 +2,21 @@ function calcSum(num) {
     let sum = 0;
     let flag = true;
 
-    if (typeof num == 'number' && num != NaN && num != Infinity) {
+    if (typeof num == 'number' && isFinite(num)) {
         sum += num;
     }
     else flag = false;
     
     function calc(arg) {
-        if (typeof arg == 'number' && arg != NaN && arg != Infinity && flag == true) {
+        if (typeof arg == 'number' && isFinite(arg) && flag == true) {
             sum += arg;
         }
-        else if (typeof arg != 'number' || arg == NaN || arg == Infinity) {
-            flag = false;
-        }
+        else flag = false;
         return calc;
     }
 
-    calc[Symbol.toPrimitive] = function(hint) {
-        if (hint == 'string') {
-            return sum;
-        }
+    calc[Symbol.toPrimitive] = function() {
+        return sum;
     }
 
     return calc;
